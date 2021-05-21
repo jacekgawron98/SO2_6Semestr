fc450ab543ff82d573de47f12f088787d89955de
Grimlaf <jacekgamer@gmail.com>
2021-05-21 12:11:01 +0200

    Finish lab4

---
+++ lab4/movies.sh
@@ -0,0 +1,211 @@
+#!/bin/bash -eu
+
+function print_help () {
+    echo "This script allows to search over movies database"
+    echo -e "-d DIRECTORY\n\tDirectory with files describing movies"
+    echo -e "-a ACTOR\n\tSearch movies that this ACTOR played in"
+    echo -e "-t QUERY\n\tSearch movies with given QUERY in title"
+    echo -e "-y YEAR\n\tSearch movies that were released in YEAR"
+    echo -e "-R QUERY\n\tSearch movies with QUERY in plot"
+    echo -e "-i \n\tSearch ignores case while using -R"
+    echo -e "-f FILENAME\n\tSaves results to file (default: results.txt)"
+    echo -e "-x\n\tPrints results in XML format"
+    echo -e "-h\n\tPrints this help message"
+}
+
+function print_error () {
+    echo -e "\e[31m\033[1m${*}\033[0m" >&2
+}
+
+function get_movies_list () {
+    local -r MOVIES_DIR=${1}
+    local -r MOVIES_LIST=$(cd "${MOVIES_DIR}" && realpath *)
+    echo "${MOVIES_LIST}"
+}
+
+function query_title () {
+    # Returns list of movies from ${1} with ${2} in title slot
+    local -r MOVIES_LIST=${1}
+    local -r QUERY=${2}
+
+    local RESULTS_LIST=()
+    for MOVIE_FILE in ${MOVIES_LIST}; do
+        if grep "| Title" "${MOVIE_FILE}" | grep -q "${QUERY}"; then
+            RESULTS_LIST+=( "${MOVIE_FILE}" )
+        fi
+    done
+    echo "${RESULTS_LIST[@]:-}"
+}
+
+function query_plot () {
+    # Returns list of movies from ${1} with query ${2} matching plot slot
+    local -r MOVIES_LIST=${1}
+    local -r QUERY=${2}
+    local -r IGNORE_CASE=${3}
+
+    local RESULTS_LIST=()
+    for MOVIE_FILE in ${MOVIES_LIST}; do
+        if ${IGNORE_CASE:-false}; then
+            if grep "| Plot" "${MOVIE_FILE}" | grep -q -i "${QUERY}"; then
+                RESULTS_LIST+=( "${MOVIE_FILE}" )
+            fi
+        else
+            if grep "| Plot" "${MOVIE_FILE}" | grep -q "${QUERY}"; then
+                RESULTS_LIST+=( "${MOVIE_FILE}" )
+            fi
+        fi
+
+    done
+    echo "${RESULTS_LIST[@]:-}"
+}
+
+function query_actor () {
+    # Returns list of movies from ${1} with ${2} in actor slot
+    local -r MOVIES_LIST=${1}
+    local -r QUERY=${2}
+
+    local RESULTS_LIST=()
+    for MOVIE_FILE in ${MOVIES_LIST}; do
+        if grep "| Actors" "${MOVIE_FILE}" | grep -q "${QUERY}"; then
+            RESULTS_LIST+=( "${MOVIE_FILE}")
+        fi
+    done
+    echo "${RESULTS_LIST[@]:-}"
+}
+
+function query_year () {
+    # Returns list of movies from ${1} with ${2} in year slot
+    local -r MOVIES_LIST=${1}
+    local -r QUERY=${2}
+
+    local RESULTS_LIST=()
+    for MOVIE_FILE in ${MOVIES_LIST}; do
+        if grep "| Year" "${MOVIE_FILE}" | grep -q "${QUERY}"; then
+            RESULTS_LIST+=( "${MOVIE_FILE}")
+        fi
+    done
+    echo "${RESULTS_LIST[@]:-}"
+}
+
+function print_xml_format () {
+    local -r FILENAME=${1}
+
+    local TEMP
+    TEMP=$(cat "${FILENAME}")
+    
+    # change 'Author:' into <Author>
+    # change others too
+    TEMP=$(echo "${TEMP}" | sed -r 's/(\|)//g')
+    TEMP=$(echo "${TEMP}" | sed -r 's/([A-Za-z]+)/<\1>/')
+    TEMP=$(echo "${TEMP}" | sed -r 's/(: )//g')
+
+    # append tag after each line
+    TEMP=$(echo "${TEMP}" | sed -r 's/([A-Za-z]+).*/\0<\/\1>/')
+    # replace first line of equals signs
+    TEMP=$(echo "${TEMP}" | sed '2 s/^.*$/<movie>/')
+    # replace the last line with </movie>
+    TEMP=$(echo "${TEMP}" | sed '$s/===*/<\/movie>/')
+
+    echo "${TEMP}"
+}
+
+function print_movies () {
+    local -r MOVIES_LIST=${1}
+    local -r OUTPUT_FORMAT=${2}
+
+    for MOVIE_FILE in ${MOVIES_LIST}; do
+        if [[ "${OUTPUT_FORMAT}" == "xml" ]]; then
+            print_xml_format "${MOVIE_FILE}"
+        else
+            cat "${MOVIE_FILE}"
+        fi
+    done
+}
+IGNORE_CASE=false
+while getopts ":hd:t:a:f:y:R:ix" OPT; do
+  case ${OPT} in
+    h)
+        print_help
+        exit 0
+        ;;
+    d)
+        MOVIES_DIR=${OPTARG}
+        ;;
+    t)
+        SEARCHING_TITLE=true
+        QUERY_TITLE=${OPTARG}
+        ;;
+    f)
+        FILE_4_SAVING_RESULTS=${OPTARG}
+        if [[ "${FILE_4_SAVING_RESULTS: -4}" != ".txt" ]]; then
+            FILE_4_SAVING_RESULTS+=".txt"
+        fi
+        ;;
+    a)
+        SEARCHING_ACTOR=true
+        QUERY_ACTOR=${OPTARG}
+        ;;
+    x)
+        OUTPUT_FORMAT="xml"
+        ;;
+    y)
+        SEARCHING_YEAR=true
+        QUERY_YEAR=${OPTARG}
+        ;;
+    R)
+        SEARCHING_IN_PLOT=true
+        QUERY_PLOT=${OPTARG}
+        ;;
+    i)
+        IGNORE_CASE=true;
+        ;;
+    \?)
+        print_error "ERROR: Invalid option: -${OPTARG}"
+        exit 1
+        ;;
+  esac
+done
+
+if [[ "${MOVIES_DIR:-}" == "" ]]; then
+    print_error "No -d option used"
+    exit 1
+fi
+
+if [[ ! -d ${MOVIES_DIR} ]]; then
+    print_error "MOVIES_DIR is not a directory"
+    exit 1
+fi
+
+MOVIES_LIST=$(get_movies_list "${MOVIES_DIR}")
+
+if ${SEARCHING_TITLE:-false}; then
+    MOVIES_LIST=$(query_title "${MOVIES_LIST}" "${QUERY_TITLE}")
+fi
+
+if ${SEARCHING_ACTOR:-false}; then
+    MOVIES_LIST=$(query_actor "${MOVIES_LIST}" "${QUERY_ACTOR}")
+fi
+
+if ${SEARCHING_YEAR:-false}; then
+    MOVIES_LIST=$(query_year "${MOVIES_LIST}" "${QUERY_YEAR}")
+fi
+
+if ${SEARCHING_IN_PLOT:-false}; then
+    MOVIES_LIST=$(query_plot "${MOVIES_LIST}" "${QUERY_PLOT}" "${IGNORE_CASE}")
+fi
+
+if [[ "${#MOVIES_LIST}" -lt 1 ]]; then
+    echo "Found 0 movies :-("
+    exit 0
+fi
+
+if [[ "${FILE_4_SAVING_RESULTS:-}" == "" ]]; then
+    print_movies "${MOVIES_LIST}" "${OUTPUT_FORMAT:-raw}"
+else
+    # TODO: add XML option
+    if [[ ${OUTPUT_FORMAT} == "xml" ]]; then
+        print_xml_format "${MOVIES_LIST}" "raw" | tee "${FILE_4_SAVING_RESULTS}"
+    else
+        print_movies "${MOVIES_LIST}" "raw" | tee "${FILE_4_SAVING_RESULTS}"
+    fi
+fi
